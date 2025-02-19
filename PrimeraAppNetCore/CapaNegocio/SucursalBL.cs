5e8ceb9b840d11221f6c2ef468a2b19da50e3b15
Davidmoran9 <davidleonel0408@gmail.com>
2025-02-19 14:48:10 -0500

    Proyecto

---
+++ PrimeraAppNetCore/CapaNegocio/SucursalBL.cs
@@ -0,0 +1,22 @@
+﻿using CapaDatos;
+using CapaEntid;
+using CapaEntidad;
+
+namespace CapaNegocio
+{
+    public class SucursalBL
+    {
+        public List<SucursalCLS> ListarSucursales()
+        {
+            SucursalDAL sucursalDAL = new SucursalDAL();
+            return sucursalDAL.ListarSucursales();
+        }
+
+        public List<SucursalCLS> FiltrarSucursales(string nombreSucursal, string direccion)
+        {
+            SucursalDAL sucursalDAL = new SucursalDAL();
+            return sucursalDAL.FiltrarSucursales(nombreSucursal, direccion);  // Enviar los parámetros de filtrado
+        }
+    }
+
+}
---
+++ PrimeraAppNetCore/PrimeraAppNetCore/Controllers/SucursalController.cs
@@ -0,0 +1,28 @@
+﻿using CapaEntid;
+using CapaEntidad;
+using CapaNegocio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeraAppNetCore.Controllers
+{
+    public class SucursalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public List<SucursalCLS> ListarSucursales()
+        {
+            SucursalBL sucursalBL = new SucursalBL();
+            return sucursalBL.ListarSucursales();
+        }
+
+        public List<SucursalCLS> FiltrarSucursales(SucursalCLS objSucursal)
+        {
+            SucursalBL sucursalBL = new SucursalBL();
+            return sucursalBL.FiltrarSucursales(objSucursal.nombre, objSucursal.direccion);  // Pasar los parámetros de forma explícita
+        }
+    }
+
+}
